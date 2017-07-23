0b2d6b24bc7376bd711adb035fd225ce48239d21
Mateo Garcia <mateog@stanford.edu>
2017-07-23 12:29:53 +0300

    Tentatively completes contact list adapter code.

--- app/src/main/java/com/mog/kontax/kontax/ContactListAdapter.java
+++ app/src/main/java/com/mog/kontax/kontax/ContactListAdapter.java
@@ -1,10 +1,17 @@
 package com.mog.kontax.kontax;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mog.kontax.kontax.databinding.ContactListItemBinding;
 
 /**
  * Created by mateogarcia on 7/20/17.
@@ -12,34 +19,33 @@ import android.widget.TextView;
 
 public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ContactListAdapterViewHolder> {
 
-    @Override
-    public ContactListAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    private static final String TAG = ContactListAdapter.class.getSimpleName();
+
+    final private ListItemClickListener mOnClickListener;
+
+    private Contact[] mContacts;
+
+    public ContactListAdapter(ListItemClickListener listener) {
+        mOnClickListener = listener;
     }
 
-    @Override
-    public void onBindViewHolder(ContactListAdapterViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
     }
 
     public class ContactListAdapterViewHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener {
 
-        ImageView icon;
-        TextView firstName;
-        TextView lastName;
+        ImageView photoImageView;
+        TextView firstNameTextView;
+        TextView lastNameTextView;
 
         public ContactListAdapterViewHolder(View itemView) {
             super(itemView);
 
-            icon = (ImageView) itemView.findViewById(R.id.personIcon);
-            firstName = (TextView) itemView.findViewById(R.id.firstName);
-            lastName = (TextView) itemView.findViewById(R.id.lastName);
+            photoImageView = (ImageView) itemView.findViewById(R.id.photoImageView);
+            firstNameTextView = (TextView) itemView.findViewById(R.id.firstNameTextView);
+            lastNameTextView = (TextView) itemView.findViewById(R.id.lastNameTextView);
 
             itemView.setOnClickListener(this);
         }
@@ -48,7 +54,46 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
         @Override
         public void onClick(View view) {
             int clickedIndex = getAdapterPosition();
-            // Toast.makeText(this, "CLICKED" + String.valueOf(clickedIndex), Toast.LENGTH_LONG).show();
+            mOnClickListener.onListItemClick(clickedIndex);
         }
+    }
+
+    @Override
+    public ContactListAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.contact_list_item;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        ContactListAdapterViewHolder viewHolder = new ContactListAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.length;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactListAdapterViewHolder contactListAdapterViewHolder,
+                                 int position) {
+        Log.d(TAG, "#" + position);
+
+        Contact contact = mContacts[position];
+        // contactListAdapterViewHolder.photoImageView = // TODO: Set photo for contact in list
+
+        String[] firstAndLastName = contact.getName().split(" ", 2);
+        String firstName = firstAndLastName[0];
+        String lastName = firstAndLastName[1];
+        contactListAdapterViewHolder.firstNameTextView.setText(firstName);
+        contactListAdapterViewHolder.firstNameTextView.setText(lastName);
+    }
+
+    public void setContacts(Contact[] contacts) {
+        mContacts = contacts;
+        notifyDataSetChanged();
     }
 }
