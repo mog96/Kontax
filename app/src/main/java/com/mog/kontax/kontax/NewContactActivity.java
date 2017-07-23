0245b5e23cc13d42b7616cd0d5760f37cb2b2c3a
Mateo Garcia <mateog@stanford.edu>
2017-07-23 08:26:53 +0300

    Adds data binding to new contact activity, saves new contact in background.

--- app/src/main/java/com/mog/kontax/kontax/NewContactActivity.java
+++ app/src/main/java/com/mog/kontax/kontax/NewContactActivity.java
@@ -1,17 +1,24 @@
 package com.mog.kontax.kontax;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.mog.kontax.kontax.databinding.ActivityNewContactBinding;
+
 public class NewContactActivity extends AppCompatActivity {
+
+    ActivityNewContactBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_new_contact);
     }
 
     @Override
@@ -25,9 +32,24 @@ public class NewContactActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_save) {
-            // TODO: Save contact
+            saveContact();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void saveContact() {
+        Contact newContact = new Contact();
+
+        String name = mBinding.nameEditText.getText().toString();
+        newContact.setName(name);
+
+        String phone = mBinding.phoneEditText.getText().toString();
+        newContact.setPhone(phone);
+
+        String email = mBinding.emailEditText.getText().toString();
+        newContact.setEmail(email);
+
+        newContact.saveInBackground();
     }
 }
