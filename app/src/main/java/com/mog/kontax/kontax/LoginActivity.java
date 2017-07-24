a87d9c5a3b8c9228794979a42d4890db7a57f9a3
Mateo Garcia <mateog@stanford.edu>
2017-07-24 23:02:26 +0300

    Deletes signup activity. Planning to add switch to toggle login/signup.

--- app/src/main/java/com/mog/kontax/kontax/LoginActivity.java
+++ app/src/main/java/com/mog/kontax/kontax/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.mog.kontax.kontax;
 
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 
 import com.mog.kontax.kontax.databinding.ActivityLoginBinding;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,9 +22,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
 
         mBinding.etEmail.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -36,14 +42,43 @@ public class LoginActivity extends AppCompatActivity {
             public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
                 if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    logIn();
+                    signUp(mBinding.etEmail.getText().toString(), mBinding.etPassword.getText().toString());
                 }
                 return false;
             }
         });
+
+        // If user is logged in, skip to main activity.
+        if (ParseUser.getCurrentUser() != null) {
+            Context context = LoginActivity.this;
+            Class destinationActivity = MainActivity.class;
+            Intent intent = new Intent(context, destinationActivity);
+            startActivity(intent);
+        }
     }
 
-    public void logIn() {
-        Toast.makeText(getApplicationContext(), "LOG IN", Toast.LENGTH_SHORT).show();
+    public void signUp(String email, String password) {
+        ParseUser user = new ParseUser();
+        user.setUsername(email);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        Toast.makeText(getApplicationContext(), "Signing up...", Toast.LENGTH_SHORT).show();
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException exception) {
+                if (exception == null) {
+                    Context context = LoginActivity.this;
+                    Class destinationActivity = MainActivity.class;
+                    Intent intent = new Intent(context, destinationActivity);
+                    startActivity(intent);
+                } else {
+                    Log.d("signup", "Error: " + exception.getMessage());
+
+                    Toast.makeText(getApplicationContext(), "Signup failed :[\nPlease try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
