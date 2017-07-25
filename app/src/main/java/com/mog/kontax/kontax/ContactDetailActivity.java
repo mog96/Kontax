47d58f5bc4e490ba9e85fee81d21332c5ce143a5
Mateo Garcia <mateog@stanford.edu>
2017-07-25 14:54:48 +0300

    Tentatively implements photo display in contact detail view.

--- app/src/main/java/com/mog/kontax/kontax/ContactDetailActivity.java
+++ app/src/main/java/com/mog/kontax/kontax/ContactDetailActivity.java
@@ -2,11 +2,16 @@ package com.mog.kontax.kontax;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.mog.kontax.kontax.databinding.ActivityContactDetailBinding;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
 
 public class ContactDetailActivity extends AppCompatActivity {
 
@@ -30,5 +35,21 @@ public class ContactDetailActivity extends AppCompatActivity {
         mBinding.tvName.setText(contact.getName());
         mBinding.tvPhone.setText(contact.getPhone());
         mBinding.tvEmail.setText(contact.getEmail());
+
+        // Load photo in background.
+        contact.getImageFile().getDataInBackground(new GetDataCallback() {
+            @Override
+            public void done(byte[] data, ParseException exception) {
+                if (exception == null) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    mBinding.ivPhoto.setImageBitmap(bitmap);
+
+                    // Access the array of results here
+                    Toast.makeText(getApplicationContext(), "Phew, loaded photo.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d("contact detail", "Error: " + exception.getMessage());
+                }
+            }
+        });
     }
 }
